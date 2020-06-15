aadf9b74b04e8bb8c967baa851f928db1dd356c5
mhmxs <kovacsricsi@gmail.com>
2020-06-15 17:16:51 +0200

    Select route reflector from the same zone first

--- topologies/multi.go
+++ topologies/multi.go
@@ -18,6 +18,7 @@ package topologies
 import (
 	"fmt"
 	"math"
+	"math/rand"
 	"strconv"
 
 	calicoApi "github.com/projectcalico/libcalico-go/lib/apis/v3"
@@ -77,16 +78,53 @@ func (t *MultiTopology) GenerateBGPPeers(routeReflectors []corev1.Node, nodes ma
 
 	bgpPeerConfigs = append(bgpPeerConfigs, *rrConfig)
 
+	peers := int(math.Min(float64(len(routeReflectors)), 3))
+
 	// TODO this could cause rebalancing very ofthen so has performance issues
-	rrIndex := 0
+	rrIndex := -1
 	for n, isReady := range nodes {
 		if !isReady || t.IsRouteReflector(string(n.GetUID()), n.GetLabels()) {
 			continue
 		}
 
-		// TODO Would be better to create RR groups [1,2,3], [1,2,4], ... to decrease number of BGP peers
-		for i := 1; i <= int(math.Min(float64(len(routeReflectors)), 3)); i++ {
+		routeReflectorsForNode := []corev1.Node{}
+
+		if t.Config.ZoneLabel != "" {
+			nodeZone := n.GetLabels()[t.Config.ZoneLabel]
+			rrsSameZone := []corev1.Node{}
+
+			for i, rr := range routeReflectors {
+				rrZone := rr.GetLabels()[t.Config.ZoneLabel]
+				if nodeZone == rrZone {
+					rrsSameZone = append(rrsSameZone, routeReflectors[i])
+				}
+			}
+
+			if len(rrsSameZone) > 0 {
+				rr := rrsSameZone[rand.Int31n(int32(len(rrsSameZone)-1))]
+				routeReflectorsForNode = append(routeReflectorsForNode, rr)
+			}
+		}
+
+		for len(routeReflectorsForNode) <= peers {
+			rrIndex++
+			if rrIndex == len(routeReflectors) {
+				rrIndex = 0
+			}
+
 			rr := routeReflectors[rrIndex]
+
+			for _, r := range routeReflectorsForNode {
+				if r.GetName() == rr.GetName() {
+					continue
+				}
+			}
+
+			routeReflectorsForNode = append(routeReflectorsForNode, rr)
+		}
+
+		// TODO Would be better to create RR groups [1,2,3], [1,2,4], ... to decrease number of BGP peers
+		for _, rr := range routeReflectorsForNode {
 			rrID := getRouteReflectorID(string(rr.GetUID()))
 			name := fmt.Sprintf(DefaultRouteReflectorClientName+"-%s", rrID, n.GetUID())
 
@@ -110,10 +148,6 @@ func (t *MultiTopology) GenerateBGPPeers(routeReflectors []corev1.Node, nodes ma
 
 			bgpPeerConfigs = append(bgpPeerConfigs, *clientConfig)
 
-			rrIndex++
-			if rrIndex == len(routeReflectors) {
-				rrIndex = 0
-			}
 		}
 	}
 
